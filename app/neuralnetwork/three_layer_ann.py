7c25a05b9caacf8937423dc838a31fe1735e07a5
seansaito <saitosean@ymail.com>
2016-03-25 04:30:43 +0800

    Modularized network script

--- app/neuralnetwork/three_layer_ann.py
+++ app/neuralnetwork/three_layer_ann.py
@@ -16,38 +16,50 @@ y = np.array([  [0],
                 [1],
                 [0]])
 
-np.random.seed(1)
+def run_net(x, y):
 
-# randomly init weights with mean 0
-syn0 = 2 * np.random.random((5,4)) - 1
-syn1 = 2 * np.random.random((4,1)) - 1
+    np.random.seed(1)
 
-for j in xrange(60000):
+    # randomly init weights with mean 0
+    syn0 = 2 * np.random.random((x.shape[1],4)) - 1
+    syn1 = 2 * np.random.random((4,1)) - 1
 
-    # Feed fowrad through layers 0, 1, and 2
-    l0 = x
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
+    for j in xrange(60000):
 
-    # calculate error
-    l2_error = y - l2
+        # Feed fowrad through layers 0, 1, and 2
+        l0 = x
+        l1 = nonlin(np.dot(l0, syn0))
+        l2 = nonlin(np.dot(l1, syn1))
 
-    if (j % 10000) == 0:
-        print "Error: " + str(np.mean(np.abs(l2_error)))
+        # calculate error
+        l2_error = y - l2
 
-
-
-    l2_delta = l2_error * nonlin(l2, deriv=True)
-
-    # how much did each l1 value contribute to the l2 error according to the weights?
-    l1_error = l2_delta.dot(syn1.T)
+        if (j % 10000) == 0:
+            print "Error: " + str(np.mean(np.abs(l2_error)))
 
 
 
-    l1_delta = l1_error * nonlin(l1, deriv=True)
+        l2_delta = l2_error * nonlin(l2, deriv=True)
 
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += l0.T.dot(l1_delta)
+        # how much did each l1 value contribute to the l2 error according to the weights?
+        l1_error = l2_delta.dot(syn1.T)
+
+
+
+        l1_delta = l1_error * nonlin(l1, deriv=True)
+
+        syn1 += l1.T.dot(l2_delta)
+        syn0 += l0.T.dot(l1_delta)
+
+    return (syn0, l1, syn1, l2)
+
+def classify(x, y, i):
+    syn0, l1, syn1, l2 = run_net(x, y)
+    res = nonlin(np.dot(nonlin(np.dot(i, syn0)), syn1))
+    print res
+    return res
+
+syn0, l1, syn1, l2 = run_net(x, y)
 
 print "End training"
 print syn0
