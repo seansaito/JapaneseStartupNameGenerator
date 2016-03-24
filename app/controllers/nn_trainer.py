2955c77df70c1a8b15019056194cb5d5c2161484
seansaito <saitosean@ymail.com>
2016-03-25 06:44:11 +0800

    Updated requirements.txt

--- app/controllers/nn_trainer.py
+++ app/controllers/nn_trainer.py
@@ -1,5 +1,9 @@
 from app.neuralnetwork.three_layer_ann import run_net, classify
+from app.ling_prepro_hrishi.core import fingerprint_word
 import numpy as np
+
+def convert_fingerprint(words):
+    return map(fingerprint_word, words)
 
 def train(x, y):
     run_net(x, y)
